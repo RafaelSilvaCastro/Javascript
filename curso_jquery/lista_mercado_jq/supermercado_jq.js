e1cf3123aa57cdb0b3c95d51ac7049cf10384c6e
RafaelSilvaCastro <165667524+RafaelSilvaCastro@users.noreply.github.com>
2024-04-15 15:33:45 -0300

    .

--- curso_jquery/lista_mercado_jq/supermercado_jq.js
+++ curso_jquery/lista_mercado_jq/supermercado_jq.js
@@ -1,3 +1,40 @@
+$(document).ready(()=>{
+    var items = [];
 
-var items = [];
+    $('input[type=submit]').click(()=>{
+        var nomeProduto = $('input[name=nome_produto]').val();
+        var precoProduto = $('input[name=price]').val();
 
+        items.push({
+            nome: nomeProduto,
+            valor: precoProduto
+        });
+
+        var listaProdutos = $('.lista-produtos');
+        var soma = 0;
+        listaProdutos.empty();
+        $.each(items, (index, val)=>{
+            soma += parseFloat(val.valor);
+            listaProdutos.append(`
+                <div class="lista-produtos-single">
+                    <h3>${val.nome}</h3>
+                    <h3 class="price-produto"><span>R$${val.valor}</span></h3>
+                </div>
+            `);
+        });
+        
+        soma = soma.toFixed(2);
+        $('input[name=nome_produto]').val("");
+        $('input[name=price]').val("");
+        
+        var elementoSoma = $('.soma-produtos h1');
+        elementoSoma.text('R$' + soma);
+    });
+
+    // Zerar o array
+    $('button[name=limpar]').click(()=>{
+        items = [];
+        $('.lista-produtos').empty();
+        $('.soma-produtos h1').text("R$0");
+    });
+});
