f8ea999c0d0bd78ed700d6a9ffb01fcb737d8817
RafaelSilvaCastro <165667524+RafaelSilvaCastro@users.noreply.github.com>
2024-04-22 14:16:17 -0300

    fim pokedex

--- curso_dankcode/pokedex/pokedex.js
+++ curso_dankcode/pokedex/pokedex.js
@@ -1,42 +1,53 @@
-fetch('https://pokeapi.co/api/v2/pokemon?limit=10')
-    .then(response => response.json())
-    .then(allpokemon => {
-
-    var pokemons = [];
-
-    allpokemon.results.map((val) => {
-        pokemons.push({nome:val.name});
-
-        fetch(val.url)
-        .then(response => response.json())
-        .then(pokemonSingle => {
-            pokemons.push({nome:val.name,image:pokemonSingle.sprites.front_defaut});
-
-            if(pokemons.length == 10){
-                //finalizamos nossas requisiçoes.
-                    var pokemonBoxes =  document.querySelector('.pokemon-boxes');
-                    pokemonBoxes.innerHTML = "";
-
-                    pokemons.map(function(val){
-                    pokemonBoxes.innerHTML += `
-                    <div class="pokemon-box">
-                        <img src="`+val.imagem+`" />
-                        <p>`+val.nome+`</p>
-                    </div>
-                `;
-
-                })
-            
-            }
-
-    })
-
-
-
-    }) 
-
-    pokemons.map((val) => {
-    console.log(val.nome);
-    })
-
+var quantidade = document.getElementById('quantidade');
+quantidade.addEventListener('keyup',()=>{
+   pegaPokemons(quantidade.value);
 })
+pegaPokemons(2);
+function pegaPokemons(quantidade){
+   fetch('https://pokeapi.co/api/v2/pokemon?limit='+quantidade)
+   .then(response => response.json())
+   .then(allpokemon => {
+
+       var pokemons = [];
+
+       allpokemon.results.map((val)=>{
+           
+           fetch(val.url)
+           .then(response => response.json())
+           .then(pokemonSingle => {
+               pokemons.push({nome:val.name,imagem:pokemonSingle.sprites.front_default});
+               
+
+               if(pokemons.length == quantidade){
+                   //Finalizamos nossas requisições.
+
+                   
+                   var pokemonBoxes = document.querySelector('.pokemon-boxes');
+                   pokemonBoxes.innerHTML = "";
+
+                  // console.log(pokemons);
+                   pokemons.map(function(val){
+                   pokemonBoxes.innerHTML+=`
+                   
+   <div class="pokemon-box">
+       <img src="`+val.imagem+`" />
+       <p>`+val.nome+`</p>
+   </div>          
+                   `;
+   
+                   })
+
+
+
+
+               }
+
+           })
+
+
+
+       })
+
+   
+   })
+}
